8d8820c707706ea06b87c39c0666b067195bce23
minhluu2608@gmail.com <minhluu2608@gmail.com>
2022-06-13 17:12:27 +0700

    cap nhat them phieuthu

--- src/Component/Receipt/ReceiptAddModal.js
+++ src/Component/Receipt/ReceiptAddModal.js
@@ -1,63 +1,114 @@
 import * as React from 'react';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import AddIcon from '@mui/icons-material/Add';
-import Stack from '@mui/material/Stack';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import TextField from "@mui/material/TextField";
 
-//stype
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
+  width: 600,
   bgcolor: 'background.paper',
-//   border: '2px solid #000',
+  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-  width: 800
 };
-export default function ReceiptAddModal() {
+
+let yearArray = [];
+
+let curYear = new Date().getFullYear();
+for (let i = curYear - 30; i <= curYear + 10; i++) {
+  yearArray.push(i);
+}
+
+export default function PhieuThuAddModal() {
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  const [khachHangList, setKhachHangList] = React.useState([]);
+  const [kyThuList, setKyThuList] = React.useState([]);
+  const [idKhachHang, setIDKhachHang] = React.useState('');
+  const [diaChiKH, setDiaChiKH] = React.useState('None');
+  const [idTuyenThu, setIDTuyenThu] = React.useState('');
+  const [idKyThu, setIDKyThu] = React.useState('');
+  const handleOpen = () => {
+    setOpen(true);
+  }
   const handleClose = () => setOpen(false);
-  const [rows, setRows] = React.useState([]);
 
-
-//tuyenthu
-  const [tuyenthu, setTuyenThu] = React.useState(0);
-  const handleChange = (event: SelectChangeEvent) => {
-    setTuyenThu(event.target.value);
+  const handleKhachHangChange = (event) => {
+    setIDKhachHang(event.target.value);
+  };
+  const handleKyThuChange = (event) => {
+    setIDKyThu(event.target.value);
   };
 
+  const handleSubmit = () => {
+    fetch("http://localhost:5199/api/phieuthu/", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "idKhachHang": idKhachHang,
+        "idTuyenThu": idTuyenThu,
+        "idKyThu": idKyThu,
+        "idNhanVien": 0,
+        "mauSoPhieu": "string",
+        "ngayTao": "string",
+        "ngayThu": "string"
+      })
+    })
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+        alert(result);
+        handleClose();
+      },
+        (error) => {
+          alert('Failed');
+        });
+  }
 
-  //Phieuthu
   React.useEffect(() => {
-    fetch("http://localhost:5199/api/PhieuThu")
+    fetch("http://localhost:5199/api/phieuthu/khachhang")
+      .then(response => response.json())
+      .then(function (customers) {
+        setKhachHangList(customers)
+      });
+    fetch("http://localhost:5199/api/phieuthu/kythu")
+      .then(response => response.json())
+      .then(function (kyThu) {
+        setKyThuList(kyThu)
+      });
+  }, [])
+
+
+  React.useEffect(() => {
+    if (idKhachHang !== '') {
+      fetch("http://localhost:5199/api/phieuthu/khachhang/" + idKhachHang)
         .then(response => response.json())
-        .then(function (PhieuThu) {
-            setRows(PhieuThu);
-        },
-            (error) => {
-                alert('Failed');
-            })
-}, [])
+        .then(function (informations) {
+          informations.map((information) => (
+            setDiaChiKH(information.Diachi),
+            setIDTuyenThu(information.IDTuyenThu)
+          ))
+        });
+    }
+  }, [idKhachHang])
+
+
+  console.log(diaChiKH);
+
   return (
     <div>
-        <Stack direction="column" spacing={2} alignItems="flex-end">
-            <Button onClick={handleOpen}
-            sx={{display: "flex", justifyContent: "flex-end"}}
-                variant="outlined" color="success"
-                startIcon={<AddIcon sx={{ fontSize: "80px" }} />}>
-
-                </Button>
-        </Stack>
+      <Stack direction="column" spacing={2} alignItems="flex-end">
+        <Button variant="contained" color="success" onClick={handleOpen}> Thêm Phiếu thu</Button>
+      </Stack>
       <Modal
         open={open}
         onClose={handleClose}
@@ -65,114 +116,49 @@ export default function ReceiptAddModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-        <Typography variant="h5" style={{ paddingBottom: 40,textAlign: "center" }}>
-                        <b>Thêm Phiếu thu</b>
-        </Typography>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 300,marginLeft:7 }}>
-            <InputLabel id="demo-simple-select-standard-label">Tuyến thu</InputLabel>
-            <Select
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-tuyenthu"
-            label="tuyenthu"
-            value={tuyenthu}
-            onChange={handleChange}
-            >
-              {rows
-                .map((row) => (
-                  <MenuItem value={row.IDTuyenThu} key={row.IDTuyenThu}>
-                    {row.TenTuyenThu}
-                  </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 300 }}>
-        <InputLabel id="demo-simple-select-standard-label">Khách hàng</InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-        <br></br>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 300,marginLeft: 7}}>
-            <InputLabel id="demo-simple-select-standard-label">Kỳ thu</InputLabel>
-            <Select
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            label="Age"
-            >
-            <MenuItem value="">
-                <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 300 }}>
-            <InputLabel id="demo-simple-select-standard-label">Nhân viên</InputLabel>
-            <Select
-            labelId="demo-simple-select-standard-label"
-            id="demo-simple-select-standard"
-            label="Age"
-            >
-            <MenuItem value="">
-                <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-        </FormControl>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 160,marginLeft: 7 }}>
-        <InputLabel id="demo-simple-select-standard-label">Mã số phiếu</InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-        <FormControl sx={{marginRight: 2}} >
-            <TextField
-                id="date"
-                label="Ngày tạo"
-                type="date"
-                sx={{ width: 220 }}
-                InputLabelProps={{
-                shrink: true,
-                }}
-            />
-        </FormControl>
-        
-        <TextField
-            id="date"
-            label="Ngày thu"
-            type="date"
-            sx={{ width: 220 }}
-            InputLabelProps={{
-            shrink: true,
-            }}
-        />
-        <Stack direction="row" spacing={2}  style={{ paddingTop: 20,float: "right"}}>
-            <Button variant="contained" >Thêm</Button>
+          <Typography variant="h5" style={{ paddingBottom: 40 }}>
+            Thêm Phiếu thu
+          </Typography>
+          <TextField
+            fullWidth
+            select
+            label="Chọn Khách Hàng"
+            value={idKhachHang}
+            onChange={handleKhachHangChange}
+            style={{ paddingBottom: 20 }}
+          >
+            {
+              khachHangList.map(customer => (
+                <MenuItem key={customer.IDKhachHang} value={customer.IDKhachHang}>{customer.HoTenKH}</MenuItem>
+              ))
+            }
+          </TextField>
+          <TextField
+            fullWidth
+            style={{ paddingBottom: 20 }}
+            label="Địa chỉ khách hàng"
+            value={diaChiKH}
+          />
+          <TextField
+            fullWidth
+            select
+            label="Chọn kỳ thu"
+            value={idKyThu}
+            onChange={handleKyThuChange}
+            style={{ paddingBottom: 20 }}
+          >
+            {
+              kyThuList.map(kythu => (
+                <MenuItem key={kythu.IDKyThu} value={kythu.IDKyThu}>{kythu.TenKyThu}</MenuItem>
+              ))
+            }
+          </TextField>
+          <Stack direction="row" spacing={2} style={{ paddingTop: 20 }}>
+            <Button variant="contained" onClick={handleSubmit}>Thêm</Button>
             <Button variant="contained" onClick={handleClose}>Huỷ bỏ</Button>
-        </Stack>
+          </Stack>
         </Box>
       </Modal>
-    </div>
-  );
+    </div >
+  )
 }
